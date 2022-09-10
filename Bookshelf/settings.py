933cc75e9940d30a36b7b21f881f733fe1623627
Gagan Garg <56231589+gagangarg631@users.noreply.github.com>
2022-09-10 17:30:59 +0530

    Update settings.py

--- Bookshelf/settings.py
+++ Bookshelf/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_4ua5(!e+d!a$7@2f&tqmplyxs)d!6hs*w5gljqqymkt=21bod'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
